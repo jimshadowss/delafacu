a028e916c494134acdd8bb96a389ed25ae2c9922
Lucas Igarzabal <lucase.igarzabal@gmail.com>
2023-01-18 13:08:54 -0300

    Probando mas con JS

--- HTML/JS/function.js
+++ HTML/JS/function.js
@@ -1,5 +1,10 @@
+
+alert(10
+    *
+    5);
 document.getElementById('boton').onclick = function () {
     console.log("Usamos el boton");
     document.getElementById('email').innerHTML = "Recibido";
-    document.getElementById('boton').innerText = "Recibido";
+    document.getElementById('form').style.display = "none";
+    alert("Recibido");
 }
