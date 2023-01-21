04d78cd17bea0654ea9639d8347e877cf2c3fb53
Lucas Igarzabal <lucase.igarzabal@gmail.com>
2023-01-21 14:19:25 -0300

    Probando bootstrap

--- HTML/JS/function.js
+++ HTML/JS/function.js
@@ -1,7 +1,5 @@
 
-alert(10
-    *
-    5);
+
 document.getElementById('boton').onclick = function () {
     console.log("Usamos el boton");
     document.getElementById('email').innerHTML = "Recibido";
