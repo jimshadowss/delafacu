a701dc3c59a24de8fde495737f5784e43728e125
Lucas Igarzabal <lucase.igarzabal@gmail.com>
2023-02-13 21:14:45 -0300

    intento

--- HTML/JS/function.js
+++
@@ -1,8 +0,0 @@
-
-
-document.getElementById('boton').onclick = function () {
-    console.log("Usamos el boton");
-    document.getElementById('email').innerHTML = "Recibido";
-    document.getElementById('form').style.display = "none";
-    alert("Recibido");
-}
--- Python/1.py
+++
@@ -1,6 +0,0 @@
-numero = 0
-print('Tabla del 3')
-while numero <= 10:
-    print(f'{numero * 3}')
-    numero += 1
-print('Fin')
